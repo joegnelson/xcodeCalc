dcb16899d7715be77d004fa9b36ce9d74ac5fa09
Joe Nelson <joe.nelson@avon.com>
2016-09-01 18:51:09 -0400

    Initial Commit

---
+++ MyCalc2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCalc2
+//
+//  Created by Joseph Nelson on 9/1/16.
+//  Copyright © 2016 Joseph Nelson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
